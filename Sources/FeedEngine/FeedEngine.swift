d692d6453d5de424ee7c595662b19c7b099cd6cd
Abin Baby <abin.baby@riskmethods.net>
2022-01-18 22:40:31 +0100

    initial commit

--- Sources/FeedEngine/FeedEngine.swift
+++
@@ -1,6 +0,0 @@
-public struct FeedEngine {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
---
+++ Sources/FeedEngine/Network/API Endpoints/GFEndPoints.swift
@@ -0,0 +1,40 @@
+//
+//  GFEndPoints.swift
+//  FeedEngine
+//
+//  Created by Abin Baby on 18/01/22.
+//
+
+import Foundation
+
+struct GFEndpoint {
+    let path: String
+    var queryItems: [URLQueryItem]?
+    var url: URL? {
+        var components: URLComponents = URLComponents()
+        components.scheme = "https"
+        components.host = Constants.baseUrl
+        components.path = path
+        components.queryItems = queryItems
+
+        return components.url
+    }
+
+    static func users(queryItems: [URLQueryItem]) -> GFEndpoint {
+        GFEndpoint(
+            path: "/search/users", queryItems: queryItems
+        )
+    }
+
+    static func userInfo(for username: String) -> GFEndpoint {
+        GFEndpoint(
+            path: "/users/\(username)", queryItems: nil
+        )
+    }
+
+    static func followersList(for username: String, queryItems: [URLQueryItem]) -> GFEndpoint {
+            GFEndpoint(
+                path: "/users/\(username)/followers", queryItems: queryItems
+            )
+        }
+}
