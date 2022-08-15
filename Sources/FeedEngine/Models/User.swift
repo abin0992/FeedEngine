acc212dcc8db04428c8206052180a03ecd41f23b
Abin Baby <abin0992@gmail.com>
2022-08-15 21:57:38 +0200

    formats naming

--- Sources/FeedEngine/Models/User.swift
+++ Sources/FeedEngine/Models/User.swift
@@ -7,19 +7,23 @@
 
 import Foundation
 
+// MARK: - UserList
+
 public struct UserList: Codable, Hashable {
     public let totalCount: Int
     public let incompleteResults: Bool
     public let items: [User]
 }
 
+// MARK: - User
+
 public struct User: Codable, Hashable {
     public var identifier: String = UUID().uuidString
     public let login: String
-    public let avatarUrl: String
+    public let avatarURL: String
 
     private enum CodingKeys: String, CodingKey {
         case login
-        case avatarUrl
-      }
+        case avatarURL
     }
+}
--- Sources/FeedEngine/Models/UserDetail.swift
+++ Sources/FeedEngine/Models/UserDetail.swift
@@ -9,13 +9,13 @@ import Foundation
 
 public struct UserDetail: Codable {
     public let login: String
-    public let avatarUrl: String
+    public let avatarURL: String
     public var name: String?
     public var location: String?
     public var bio: String?
     public let publicRepos: Int
     public let publicGists: Int
-    public let htmlUrl: String
+    public let htmlURL: String
     public let following: Int
     public let followers: Int
     public let createdAt: Date
