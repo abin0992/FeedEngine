354ed6f235a72b0f94a861ded5e77bbac8250e49
Abin Baby <abin0992@gmail.com>
2022-09-22 22:15:00 +0200

    Fix

--- Sources/FeedEngine/Models/UserDetail.swift
+++ Sources/FeedEngine/Models/UserDetail.swift
@@ -20,7 +20,7 @@ public struct UserDetail: Codable {
     let followers: Int
     let createdAt: Date
     
-    init(login: String, avatarUrl: String, name: String? = nil, location: String? = nil, bio: String? = nil, publicRepos: Int, publicGists: Int, htmlUrl: String, following: Int, followers: Int, createdAt: Date) {
+    public init(login: String, avatarUrl: String, name: String? = nil, location: String? = nil, bio: String? = nil, publicRepos: Int, publicGists: Int, htmlUrl: String, following: Int, followers: Int, createdAt: Date) {
         self.login = login
         self.avatarUrl = avatarUrl
         self.name = name
