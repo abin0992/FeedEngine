dc265be71c900c8d6084d2bc7b98118ad3e4fa7b
Abin Baby <abin0992@gmail.com>
2022-09-22 22:09:50 +0200

    Adds  memberwise init

--- Sources/FeedEngine/Models/UserDetail.swift
+++ Sources/FeedEngine/Models/UserDetail.swift
@@ -19,4 +19,18 @@ public struct UserDetail: Codable {
     let following: Int
     let followers: Int
     let createdAt: Date
+    
+    init(login: String, avatarUrl: String, name: String? = nil, location: String? = nil, bio: String? = nil, publicRepos: Int, publicGists: Int, htmlUrl: String, following: Int, followers: Int, createdAt: Date) {
+        self.login = login
+        self.avatarUrl = avatarUrl
+        self.name = name
+        self.location = location
+        self.bio = bio
+        self.publicRepos = publicRepos
+        self.publicGists = publicGists
+        self.htmlUrl = htmlUrl
+        self.following = following
+        self.followers = followers
+        self.createdAt = createdAt
+    }
 }
