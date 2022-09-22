28e52ff2197f5d4fc74681ba84bbdfffe3f32d76
Abin Baby <abin0992@gmail.com>
2022-09-22 22:04:35 +0200

    adds memberwise initialiser

--- Sources/FeedEngine/Models/User.swift
+++ Sources/FeedEngine/Models/User.swift
@@ -11,12 +11,24 @@ public struct UserList: Codable, Hashable {
     let totalCount: Int
     let incompleteResults: Bool
     let items: [User]
+    
+    public init(totalCount: Int, incompleteResults: Bool, items: [User]) {
+        self.totalCount = totalCount
+        self.incompleteResults = incompleteResults
+        self.items = items
+    }
 }
 
 public struct User: Codable, Hashable {
     var identifier: String = UUID().uuidString
     let login: String
     let avatarUrl: String
+
+    public init(identifier: String, login: String, avatarUrl: String) {
+        self.identifier = identifier
+        self.login = login
+        self.avatarUrl = avatarUrl
+    }
 
     private enum CodingKeys: String, CodingKey {
         case login
